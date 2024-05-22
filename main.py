92965429c4118a89d7e0c43ac3407811e1e6cbc0
Sergey <pristup2040@gmail.com>
2024-05-22 20:40:52 +1000

    update InsectDAO.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from repository import InsectDAO, SquadDAO, FamilyDAO
 
 if __name__ == "__main__":
-    # a = InsectDAO()
-    #
+    a = InsectDAO()
+
+    a.update_insect(10, img="/jh")
+
     # print(a.select_all())
     #
     # print(a.count())
 
-    s = SquadDAO()
+    # s = SquadDAO()
     # s.add_squad("sd")
     # s.add_squad("sd2")
     # s.add_squad("sd2")
 
-    print(s.select_all())
-
-    f = FamilyDAO()
-
-    f.add_squad("TEST F23", 2)
-
-    print(f.select_all())
+    # print(s.select_all())
+    #
+    # f = FamilyDAO()
+    #
+    # f.add_squad("TEST F23", 2)
+    #
+    # print(f.select_all())
 
 
--- repository/InsectDAO.py
+++ repository/InsectDAO.py
@@ -5,13 +5,33 @@ class InsectDAO(AbstractTableDAO):
     def __init__(self):
         super().__init__("insect")
 
-    def add_insect(self, name):
-        sql = f"INSERT INTO {self.table_name} (name) VALUES (%s)"
-        parameters = (name, )
+    def add_insect(self, lat_name: str, ru_name: str, family_id: int):
+        sql = f"INSERT INTO {self.table_name} (lat_name, ru_name, family_id) VALUES (%s, %s, %s)"
+        parameters = (lat_name, ru_name, family_id)
         self._execute(sql, parameters=parameters, commit=True)
-        return True
+        # print("id: ", a)
 
-    def update_insect(self, password_id, new_password):
-        sql = f"UPDATE {self.table_name} SET password=? WHERE id=?"
-        self._execute(sql, parameters=(new_password, password_id), commit=True)
+    @staticmethod
+    def _format_args_to_update(sql, parameters: dict):
+        # используется для создания sql команды с нужными параметрами для команды ниже
+        sql += ' , '.join([
+            f"{item} = %s" for item in parameters.keys()
+        ])
+        return sql, tuple(parameters.values())
+
+    def select_one(self, **kwargs):
+        sql = f'SELECT * FROM {self.table_name} WHERE '
+        sql, parameters = self._format_args(sql, kwargs)
+        return self._execute(sql, parameters, fetchone=True)
+        # пример использования команды select_desk(id=131, name='JoJo')
+
+    def update_insect(self, id_, **kwargs):
+        sql = f'UPDATE {self.table_name} SET '
+        sql, parameters = self._format_args_to_update(sql, kwargs)
+        sql += " WHERE id = %s"
+        parameters = parameters + (id_, )
+        # print(parameters)
+        # print(sql)
+
+        self._execute(sql, parameters=parameters, commit=True)
 
