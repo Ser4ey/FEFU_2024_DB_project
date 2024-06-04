c3c65bb4c4f3e6680c2bf36cb63763c164eecfdf
Sergey <pristup2040@gmail.com>
2024-06-04 17:59:49 +1000

    add insects_catalog.html

--- SiteProject/app.py
+++ SiteProject/app.py
@@ -6,32 +6,27 @@ app = Flask(__name__)
 # Initialize the repository
 repo = Repository()
 
-insects = (
-    {"id": 3},
-    {"id": 4},
-)
-
 
 @app.route('/')
 def index():
-    insects = repo.get_insects(limit=10, offset=0)
-    return render_template('index.html', insects=insects)
+    insects = repo.get_all_insects()
+    return render_template('insects_catalog.html', insects=insects)
 
-
-@app.route('/insect/<int:id>')
-def insect(id):
-    insect = repo.get_insect(id)
-    return render_template('insect.html', insect=insect)
-
-
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    if request.method == 'POST':
-        squad_id = request.form.get('squad_id')
-        family_id = request.form.get('family_id')
-        insects = repo.search_insects(squad_id, family_id)
-        return render_template('index.html', insects=insects)
-    return render_template('index.html')
+#
+# @app.route('/insect/<int:id>')
+# def insect(id):
+#     insect = repo.get_insect(id)
+#     return render_template('insect.html', insect=insect)
+#
+#
+# @app.route('/search', methods=['GET', 'POST'])
+# def search():
+#     if request.method == 'POST':
+#         squad_id = request.form.get('squad_id')
+#         family_id = request.form.get('family_id')
+#         insects = repo.search_insects(squad_id, family_id)
+#         return render_template('index.html', insects=insects)
+#     return render_template('index.html')
 
 
 if __name__ == '__main__':
--- SiteProject/repository.py
+++ SiteProject/repository.py
@@ -1,8 +1,9 @@
 import psycopg2
 
+
 class Repository:
     def __init__(self):
-        self.conn = psycopg2.connect(
+        self.connection = psycopg2.connect(
             dbname="scrapper",
             user="postgres",
             password="postgres",
@@ -10,21 +11,78 @@ class Repository:
             port="5432"
         )
 
-        self.cur = self.conn.cursor()
+    def _execute(self, sql: str, parameters: tuple = None, fetchone=False, fetchall=False, commit=False):
+        if not parameters:
+            parameters = tuple()
 
-    def get_insects(self, limit, offset):
-        self.cur.execute("SELECT * FROM insect ORDER BY id LIMIT %s OFFSET %s", (limit, offset))
-        return self.cur.fetchall()
+        # connection.set_trace_callback(logger)
+        cursor = self.connection.cursor()
+        cursor.execute(sql, parameters)
+        data = None
 
-    def get_insect(self, id):
-        self.cur.execute("SELECT * FROM insect WHERE id = %s", (id,))
-        return self.cur.fetchone()
+        if commit:
+            self.connection.commit()
+        if fetchone:
+            data = cursor.fetchone()
+        if fetchall:
+            data = cursor.fetchall()
+        # self.connection.close()
 
-    def search_insects(self, squad_id, family_id):
-        query = "SELECT * FROM insect"
-        if squad_id:
-            query += " WHERE family_id IN (SELECT id FROM family WHERE squad_id = %s)" % squad_id
-        if family_id:
-            query += " WHERE family_id = %s" % family_id
-        self.cur.execute(query)
-        return self.cur.fetchall()
+        return data
+
+    def get_all_insects(self):
+        sql = """
+        SELECT
+        insect.id AS insect_id,
+        lat_name,
+        ru_name,
+        img,
+        
+        squad.id AS squad_id,
+        squad.name AS squad_name,
+        family.id AS family_id,
+        family.name AS family_name,
+
+        description,
+        category_and_status,
+        distribution,
+        area, 
+        habitat, 
+        limiting_factors,
+        count_ AS count,
+        security_notes 
+        
+        FROM insect 
+            JOIN family ON insect.family_id = family.id
+            JOIN squad ON family.squad_id = squad.id
+        """
+        insects = self._execute(sql, fetchall=True)
+        return [Insect(*insect) for insect in insects]
+
+
+class Insect:
+    def __init__(self, insect_id, lat_name, ru_name, img, squad_id, squad_name, family_id, family_name, description, category_and_status, distribution, area, habitat, limiting_factors, count, security_notes):
+        self.insect_id = insect_id
+        self.lat_name = lat_name
+        self.ru_name = ru_name
+        self.img = img
+        self.squad_id = squad_id
+        self.squad_name = squad_name
+        self.family_id = family_id
+        self.family_name = family_name
+        self.description = description
+        self.category_and_status = category_and_status
+        self.distribution = distribution
+        self.area = area
+        self.habitat = habitat
+        self.limiting_factors = limiting_factors
+        self.count = count
+        self.security_notes = security_notes
+
+    def __str__(self):
+        return f"Insect {self.lat_name} (ID: {self.insect_id}) - {self.family_name} ({self.squad_name})"
+
+
+if __name__ == "__main__":
+    r = Repository()
+    print(r.get_all_insects()[0])
