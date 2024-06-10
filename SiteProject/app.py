dbafb39cbf766dd103321238e909e4f58d91aeab
Sergey <pristup2040@gmail.com>
2024-06-10 19:43:22 +1000

    v 1.5 some style

--- SiteProject/app.py
+++ SiteProject/app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from repository import Repository
 
 app = Flask(__name__)
 
-# Initialize the repository
 repo = Repository()
 
 
@@ -17,17 +16,8 @@ def index():
 def insect(id_):
     insect = repo.get_insect(id_)
     return render_template('insect_page.html', insect=insect)
-#
-#
-# @app.route('/search', methods=['GET', 'POST'])
-# def search():
-#     if request.method == 'POST':
-#         squad_id = request.form.get('squad_id')
-#         family_id = request.form.get('family_id')
-#         insects = repo.search_insects(squad_id, family_id)
-#         return render_template('index.html', insects=insects)
-#     return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
